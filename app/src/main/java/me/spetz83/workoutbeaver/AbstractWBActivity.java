9552ee32b58365be0a550574a066072381b5db23
Tom Etzel <tom@blueearth.net>
2015-01-07 23:11:13 -0600

    adding lollipop theme support

---
+++ app/src/main/java/me/spetz83/workoutbeaver/AbstractWBActivity.java
@@ -0,0 +1,23 @@
+package me.spetz83.workoutbeaver;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by tom on 1/7/2015.
+ */
+public class AbstractWBActivity extends ActionBarActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(toolbar != null)
+        {
+            setSupportActionBar(toolbar);
+        }
+    }
+}
