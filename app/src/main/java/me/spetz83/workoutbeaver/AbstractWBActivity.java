eb5dbae4b99c86ab4d425fa70325928a861f3032
Tom Etzel <tom@blueearth.net>
2015-01-14 14:38:11 -0600

    added google sign in button and methods needed to trigger sign in.

--- app/src/main/java/me/spetz83/workoutbeaver/AbstractWBActivity.java
+++ app/src/main/java/me/spetz83/workoutbeaver/AbstractWBActivity.java
@@ -3,9 +3,7 @@ package me.spetz83.workoutbeaver;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-
 import com.parse.Parse;
-import com.squareup.otto.Bus;
 
 /**
  * Created by tom on 1/7/2015.
@@ -24,6 +22,5 @@ public class AbstractWBActivity extends ActionBarActivity
         }
 
         Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
-
     }
 }
--- app/src/main/java/me/spetz83/workoutbeaver/MainActivity.java
+++ app/src/main/java/me/spetz83/workoutbeaver/MainActivity.java
@@ -1,15 +1,17 @@
 package me.spetz83.workoutbeaver;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
 import com.squareup.otto.Bus;
 
 import javax.inject.Inject;
@@ -17,14 +19,20 @@ import javax.inject.Inject;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
-import me.spetz83.workoutbeaver.events.AuthEvent;
 
 
-public class MainActivity extends AbstractWBActivity
+public class MainActivity extends AbstractWBActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener
 {
     private static final String MAIN_TAG = "MAIN_ACT";
-
     public static final String AUTH_MESSAGE = "AUTH_MESSAGE";
+    private static final int RC_SIGN_IN = 0;
+
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mIntentInProgress;
+    private boolean mGplusClicked;
+    private ConnectionResult mConnectionResult;
 
     @Inject
     Bus bus;
@@ -42,6 +50,13 @@ public class MainActivity extends AbstractWBActivity
         super.onCreate(savedInstanceState);
         DaggerInjector.inject(this);
         ButterKnife.inject(this);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN)
+                .build();
     }
 
     @OnClick(R.id.btn_login)
@@ -55,10 +70,14 @@ public class MainActivity extends AbstractWBActivity
         startActivity(intent);
     }
 
-    @OnClick(R.id.btn_register)
-    public void register(View view)
+    @OnClick(R.id.btn_gplus)
+    public void gPlusSignIn(View view)
     {
-        Log.d(MAIN_TAG, "Register");
+        if(!mGoogleApiClient.isConnecting())
+        {
+            mGplusClicked = true;
+            resolveSignInError();
+        }
     }
 
     @Override
@@ -73,6 +92,85 @@ public class MainActivity extends AbstractWBActivity
     {
         super.onPause();
         bus.unregister(this);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        if(mGoogleApiClient.isConnected())
+        {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle)
+    {
+        mGplusClicked = false;
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause)
+    {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult)
+    {
+        if(!mIntentInProgress)
+        {
+            mConnectionResult = connectionResult;
+
+            if(mGplusClicked)
+            {
+                resolveSignInError();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent)
+    {
+        if(requestCode == RC_SIGN_IN)
+        {
+            if(responseCode != RESULT_OK)
+            {
+                mGplusClicked = false;
+            }
+            mIntentInProgress = false;
+            if(!mGoogleApiClient.isConnecting())
+            {
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    private void resolveSignInError()
+    {
+        if(!mIntentInProgress && mConnectionResult.hasResolution())
+        {
+            try
+            {
+                mIntentInProgress = true;
+                startIntentSenderForResult(mConnectionResult.getResolution().getIntentSender(),
+                        RC_SIGN_IN, null, 0, 0, 0);
+            }
+            catch (IntentSender.SendIntentException e)
+            {
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
     }
 
     @Override
